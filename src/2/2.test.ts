0d40c39ebf4232eeaf4ea1073b00a6b43cf53993
Jakub Dlubek <jakub.dlubek@fanduel.com>
2024-12-02 12:00:31 +0000

    add levelsAreGraduallyChanging and levelsAreEitherDecreasingOrIncreasing functions

--- src/2/2.test.ts
+++ src/2/2.test.ts
@@ -1,20 +1,42 @@
-import { expect, describe, it } from "bun:test";
+import { expect, describe, it, test } from "bun:test";
 import { getTestInput } from "../../getInput";
-import { parseInput } from "./2";
+import {
+  levelsAreEitherDecreasingOrIncreasing,
+  levelsAreGraduallyChanging,
+  parseInput,
+} from "./2";
 const testInput = await getTestInput("2");
-
-const expectedParsedInput = [
-  [7, 6, 4, 2, 1],
-  [1, 2, 7, 8, 9],
-  [9, 7, 6, 2, 1],
-  [1, 3, 2, 4, 5],
-  [8, 6, 4, 4, 1],
-  [1, 3, 6, 7, 9],
-];
 
 describe("Day 2", () => {
   it("should parse input (each row is a list of levels", () => {
+    const expectedParsedInput = [
+      [7, 6, 4, 2, 1],
+      [1, 2, 7, 8, 9],
+      [9, 7, 6, 2, 1],
+      [1, 3, 2, 4, 5],
+      [8, 6, 4, 4, 1],
+      [1, 3, 6, 7, 9],
+    ];
+
     parseInput(testInput);
     expect(parseInput(testInput)).toEqual(expectedParsedInput);
+  });
+
+  it("should return true if levels in a row are either decreasing or increasing", () => {
+    const parsedInput = parseInput(testInput);
+    const expectedResult = [true, true, true, false, false, true];
+    const result = parsedInput.map((row) =>
+      levelsAreEitherDecreasingOrIncreasing(row)
+    );
+    expect(result).toEqual(expectedResult);
+  });
+
+  it("should return true if levels are gradually stepped", () => {
+    const parsedInput = parseInput(testInput);
+    const expectedResult = [true, false, false, true, false, true];
+    const result = parsedInput.map((row) =>
+      levelsAreGraduallyChanging(row, 3, 1)
+    );
+    expect(result).toEqual(expectedResult);
   });
 });
--- src/2/2.ts
+++ src/2/2.ts
@@ -1,13 +1,49 @@
-// Advent of Code 2024 - Day 
+// Advent of Code 2024 - Day
 
 export function parseInput(inputRaw: string): number[][] {
-    return inputRaw.split('\n').map((row)=> {
-        return row.split(' ').map((level) => parseInt(level));
-    })
+  return inputRaw.split("\n").map((row) => {
+    return row.split(" ").map((level) => parseInt(level));
+  });
 }
 
-export function getAnswer(inputRaw: string) {
+export function levelsAreEitherDecreasingOrIncreasing(row: number[]): boolean {
+  let levelsAreDecreasing = true;
+  let levelsAreIncreasing = true;
 
+  for (let i = 0; i < row.length; i++) {
+    if (row[i] <= row[i + 1]) {
+      levelsAreDecreasing = false;
+      break;
+    }
+  }
+
+  for (let i = 0; i < row.length; i++) {
+    if (row[i] >= row[i + 1]) {
+      levelsAreIncreasing = false;
+      break;
+    }
+  }
+
+  return levelsAreDecreasing || levelsAreIncreasing;
 }
 
+export function levelsAreGraduallyChanging(
+  row: number[],
+  maxDifference: number,
+  minDistance: number
+): boolean {
+  let levelsAreGraduallyIncreasing = true;
 
+  for (let i = 0; i < row.length; i++) {
+    if (
+      Math.abs(row[i] - row[i + 1]) > maxDifference ||
+      Math.abs(row[i] - row[i + 1]) < minDistance
+    ) {
+      levelsAreGraduallyIncreasing = false;
+    }
+  }
+
+  return levelsAreGraduallyIncreasing;
+}
+
+export function getAnswer(inputRaw: string) {}
