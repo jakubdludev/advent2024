8ddf94ac252e81e6fd65dfa8b0dcc73f8ed6f570
jakubdlu.dev <contact@jakubdlu.dev>
2024-12-08 08:25:30 +0000

    day 5 part 1, dirty

--- src/5/5.test.ts
+++ src/5/5.test.ts
@@ -1,6 +1,7 @@
+
 import { expect, describe, it } from "bun:test";
 import { getTestInput } from "../../getInput";
-import { getAnswerA, getAnswerB, parseInput } from "./5";
+import { checkPageOrderingRules, checkSinglePageNumbersUpdate, getAnswerA, getAnswerB, isAbeforeBInArray, parseInput } from "./5";
 
 const testInput = await getTestInput("4");
 
@@ -53,4 +54,60 @@ describe("Day 5 functions", () => {
 
     expect(getAnswerB(parsedInput)).toEqual(0);
   });
+
+  it("isAbeforeBInArray should return true if a is before b in the array", () => {
+    const array = [1, 2, 3, 4, 5];
+    expect(isAbeforeBInArray(array, 1, 2)).toBe(true);
+    expect(isAbeforeBInArray(array, 2, 1)).toBe(false);
+    expect(isAbeforeBInArray(array, 3, 2)).toBe(false);
+  });
+
+  it("checkSinglePageNumbersUpdate should return true if the page ordering rules are satisfied", () => {
+    const pageNumbersUpdate = [75, 47, 61, 53, 29];
+    const pageOrderingRules = [
+      [47, 53],
+      [97, 13],
+      [97, 61],
+      [97, 47],
+      [75, 29],
+      [61, 13],
+      [75, 53],
+      [29, 13],
+      [97, 29],
+      [53, 29],
+      [61, 53],
+      [97, 53],
+      [61, 29],
+      [47, 13],
+      [75, 47],
+      [97, 75],
+      [47, 61],
+      [75, 61],
+      [47, 29],
+      [75, 13],
+      [53, 13],
+    ];
+    expect(checkSinglePageNumbersUpdate(pageNumbersUpdate, pageOrderingRules)).toBe(true);
+  });
+
+  it("checkSinglePageNumbersUpdate should return false if the page ordering rules are not satisfied", () => {
+    const parsedInput = parseInput(testInput);
+    const page = parsedInput.pageNumbersOfEachUpdate[3];
+    const rules = parsedInput.pageOrderingRules;
+
+    expect(checkSinglePageNumbersUpdate(page, rules)).toBe(false);
+  });
+
+  it("checkPageOrderingRules should return true if the page ordering rules are satisfied", () => {
+    const parsedInput = parseInput(testInput);
+    expect(checkPageOrderingRules(parsedInput)).toEqual([
+      true,
+      true,
+      true,
+      false,
+      false,
+      false,
+    ]);
+  });
+  
 });
--- src/5/5.ts
+++ src/5/5.ts
@@ -3,52 +3,87 @@
 import type { Answer } from "../../types";
 
 export interface Input {
-  pageOrderingRules: number[][];
-  pageNumbersOfEachUpdate: number[][];
+    pageOrderingRules: number[][];
+    pageNumbersOfEachUpdate: number[][];
 }
 
-export function parseInput(inputRaw: string): Input  {
-  const lines = inputRaw.split("\n");
-  const lastPageOrderingRulesLine = lines.findIndex((line) => line === "");
-  const pageOrderingRules = lines.slice(0, lastPageOrderingRulesLine).map((line) => line.split("|")).map(line=>line.map((number)=>parseInt(number)));
-  const pageNumbersOfEachUpdate = lines.slice(lastPageOrderingRulesLine+1).map((line) => line.split(",")).map((line) => line.map((number) => parseInt(number)));  
+export function parseInput(inputRaw: string): Input {
+    const lines = inputRaw.split("\n");
+    const lastPageOrderingRulesLine = lines.findIndex((line) => line === "");
+    const pageOrderingRules = lines
+        .slice(0, lastPageOrderingRulesLine)
+        .map((line) => line.split("|"))
+        .map((line) => line.map((number) => parseInt(number)));
+    const pageNumbersOfEachUpdate = lines
+        .slice(lastPageOrderingRulesLine + 1)
+        .map((line) => line.split(","))
+        .map((line) => line.map((number) => parseInt(number)));
 
-  
-  return {
-    pageOrderingRules,
-    pageNumbersOfEachUpdate
-  }
+    return {
+        pageOrderingRules,
+        pageNumbersOfEachUpdate,
+    };
 }
 
-export function findRelevantRules(printingSection: number[], allRules: number[][]): number[][] {
-  return allRules.filter((rule) => {
-    // console.log(rule);
-    // console.log(printingSection);
-    // console.log('-----');
-    const relevant =  rule.every((pageNumber) => printingSection.includes(pageNumber));
+export function isAbeforeBInArray(array: number[], a: number, b: number): boolean {
+    if (array.indexOf(a) === -1 || array.indexOf(b) === -1) {
+        return true;
+    }
+    const indexA = array.indexOf(a);
+    const indexB = array.indexOf(b);
+    return indexA < indexB;
+};
 
-    console.log(relevant);
-  });    
+export function checkPageOrderingRules(parsedInput: Input): boolean[] {
+  const answer = parsedInput.pageNumbersOfEachUpdate.map(
+    (pageNumbersOfEachUpdate) => {
+      let isCorrect = true;
+        pageNumbersOfEachUpdate.forEach((pageNumber, index) => {
+          parsedInput.pageOrderingRules.forEach((pageOrderingRule) => {
+            if(pageNumbersOfEachUpdate.slice(index).includes(pageOrderingRule[0]) && pageNumbersOfEachUpdate.includes(pageOrderingRule[1])) {
+              if(!isAbeforeBInArray(pageNumbersOfEachUpdate, pageNumber, pageOrderingRule[1])) {
+                isCorrect = false;
+              }
+            }
+          });
+        });
+        return isCorrect;
+    });
+
+    return answer;
 }
+
+export function checkSinglePageNumbersUpdate(pageNumbersUpdate: number[], pageOrderingRules: number[][]): boolean {
+  let isCorrect = true;
+    pageNumbersUpdate.forEach((pageNumber, index) => {
+      pageOrderingRules.forEach((pageOrderingRule) => {
+        if(pageNumbersUpdate.slice(index).includes(pageOrderingRule[0]) && pageNumbersUpdate.includes(pageOrderingRule[1])) {
+          if(!isAbeforeBInArray(pageNumbersUpdate, pageNumber, pageOrderingRule[1])) {
+            isCorrect = false;
+          }
+        }
+      });
+    });
+    return isCorrect;
+}
+
 
 
 export function getAnswerA(parsedInput: Input): number {
-  findRelevantRules(parsedInput.pageNumbersOfEachUpdate[0], parsedInput.pageOrderingRules);
-
-  return 0;
+    const onlyValidPages = parsedInput.pageNumbersOfEachUpdate.filter((pageNumbersUpdate) => checkSinglePageNumbersUpdate(pageNumbersUpdate, parsedInput.pageOrderingRules));
+    const middleValuesFromValidPages = onlyValidPages.map((pageNumbersUpdate) => pageNumbersUpdate[Math.floor(pageNumbersUpdate.length / 2)]);
+    return middleValuesFromValidPages.reduce((a, b) => a + b, 0);
 }
 
-
-
 export function getAnswerB(parsedInput: Input): number {
-  return 0;
+    return 0;
 }
 
 export function getAnswer(inputRaw: string): Answer {
-  const parsedInput = parseInput(inputRaw);
+    const parsedInput = parseInput(inputRaw);
 
-  return {
-    a: getAnswerA(parsedInput),
-    b: getAnswerB(parsedInput),
-  };
+    return {
+        a: getAnswerA(parsedInput),
+        b: getAnswerB(parsedInput),
+    };
 }
